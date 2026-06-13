e26121f579a8ce79ef289bc794a9a12da0f58abc
Pratyush <atamitystudent@gmail.com>
2026-06-14 00:29:57 +0530

    Fix CORS policy for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   credentials: true,
 }));
 app.use(express.json());
